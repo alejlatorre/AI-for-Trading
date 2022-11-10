db518ca7a4b647b92f80a63f86ab65483219bc5e
alejlatorre <alejandro.latorre@pucp.edu.pe>
2022-11-10 00:57:04 -0500

    added count_words script in new project folder and added ipykernel into requirements

---
+++ ai-algorithms-in-trading/01. count_words.py
@@ -0,0 +1,39 @@
+"""Count words."""
+import re
+from collections import Counter
+
+
+def count_words(text):
+    """Count how many times each unique word occurs in text."""
+    counts = dict()  # dictionary of { <word>: <count> } pairs to return
+    
+    # TODO: Convert to lowercase
+    text = text.lower()
+    
+    # TODO: Split text into tokens (words), leaving out punctuation
+    alpha_text = re.sub('[^a-z ]','', text)
+    tokens = re.split('[^a-z]', alpha_text) 
+    
+    # TODO: Aggregate word counts using a dictionary
+    counts.update(dict(Counter(tokens)))
+
+    return counts
+
+
+def test_run():
+    with open("data/input.txt", "r") as f:
+        text = f.read()
+        counts = count_words(text)
+        sorted_counts = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
+        
+        print("10 most common words:\nWord\tCount")
+        for word, count in sorted_counts[:10]:
+            print("{}\t{}".format(word, count))
+        
+        print("\n10 least common words:\nWord\tCount")
+        for word, count in sorted_counts[-10:]:
+            print("{}\t{}".format(word, count))
+
+
+if __name__ == "__main__":
+    test_run()
